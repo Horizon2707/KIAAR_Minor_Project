4c526d855c0fb1e8d5c12547fb99e482dd687950
DevPanchal1 <145889118+DevPanchal1@users.noreply.github.com>
2024-02-19 22:54:51 +0530

    Adding the unfinished UI

---
+++ ui/src/Components/App.js
@@ -0,0 +1,13 @@
+import "../Styles/App.css"
+import { Form } from "./Form";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{marginTop:'2vh'}}>Soil Water Test Entry Form</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ ui/src/Components/Form.js
@@ -0,0 +1,111 @@
+import "../Styles/Form.css";
+import { Select, Input, Button } from "@chakra-ui/react";
+export function Form() {
+  const obj = {
+    width: "20vh",
+  };
+  return (
+    <>
+      <div className="container">
+        <div className="row1 common">
+          <div className=" item">
+            <h4>
+              <label htmlFor="test">Test Type</label>
+            </h4>
+            <Select
+              style={obj}
+              variant="filled"
+              size="md"
+              placeholder="Select one..."
+              id="test"
+            >
+              <option value="waterTest">Water Test</option>
+              <option value="soilTest">Soil Test</option>
+            </Select>
+          </div>
+          <div className="item">
+            <h4>
+              <label>Tamplete No:-</label>
+            </h4>
+            x
+          </div>
+          <div className="item">
+            <h4>
+              <label htmlFor="labNo">Lab No.</label>
+            </h4>
+            xx
+          </div>
+        </div>
+        <div className="row2 common">
+          <div className="farmer_id item centering">
+            <h4>
+              <label htmlFor="farmerId">Farmer ID</label>
+            </h4>
+            <Input
+              size="md"
+              variant="filled"
+              placeholder="Enter 6 digits Farmer Id"
+            />
+          </div>
+          <div className="item">
+            <h5>xnxxxaxxxxmxxex</h5>
+          </div>
+          <div className="item">
+            <h5>MOBIL no. :</h5>
+            <h6>xxxxxxxxxx</h6>
+          </div>
+        </div>
+        <div className="row3 common">
+          <div className="item">
+            P Address:
+            xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+          </div>
+        </div>
+        <div className="row4 common">
+          <div className="item">Cluster:xxxxxxxxxxxx</div>
+          <div className="item">Village:xxxxxxxxxxxxx</div>
+          <div className="item">Plot No.:xxxxxx</div>
+          <div className="item">Area:xxxxx</div>
+        </div>
+        <div className="row5 common">
+          <div className="item">
+            <h4>Drainage : xxxx</h4>
+          </div>
+          <div className="item">
+            <h4>Soil Type : xxxx</h4>
+          </div>
+          <div className="item">
+            <h4>Water Type : xxxx</h4>
+          </div>
+          <div className="item">
+            <h4>Irrigation Source : xxxx</h4>
+          </div>
+          <div className="item">
+            <h4>Cultivation Type : xxxx</h4>
+          </div>
+        </div>
+        <div className="row6 common">
+          <div className="item">
+            <h4>Previous Crop : xxxx</h4>
+          </div>
+          <div className="item">
+            <h4>Crop to be grown : xxxx</h4>
+          </div>
+          <div className="item">
+            <h4>Dt of Sampling : xxxx</h4>
+          </div>
+          <div className="item">
+            <h4>Dt of Sampling Receipt : xxxx</h4>
+          </div>
+        </div>
+      </div>
+      <br />
+      <hr />
+      <div className="centering">
+        <Button colorScheme='whatsapp' size="md">
+          Show the crop to be grown
+        </Button>
+      </div>
+    </>
+  );
+}
