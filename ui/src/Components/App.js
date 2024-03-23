bc2d9931cf85cd9c763e17517180a0624dda2061
DevPanchal1 <145889118+DevPanchal1@users.noreply.github.com>
2024-03-23 10:39:04 +0000

    ok

--- ui/src/Components/App.js
+++ ui/src/Components/App.js
@@ -1,19 +1,21 @@
 import "../Styles/App.css";
-import { Form } from "./Form";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
+import { Form } from "./Form";
 import ResultEntry from "./ResultEntry";
 import NotDefined from "./NotDefined";
 import Login from "./Login";
 import Sign from "./Sign";
+
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/form" element={<Form />} />
           <Route path="/" element={<Login />} />
           <Route path="/signUp" element={<Sign />} />
-          <Route path="/resultentry" element={<ResultEntry />} />
+          <PrivateRoute path="/form" element={Form} />
+          <PrivateRoute path="/resultentry" element={ResultEntry} />
           <Route path="*" element={<NotDefined />} />
         </Routes>
       </Router>
